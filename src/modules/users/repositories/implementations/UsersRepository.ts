959e8e56e2b993e1e42171adfb1ab09928bd4d3a
jonathantvrs <jonathan.tavares.silva@ccc.ufcg.edu.br>
2022-12-07 22:27:51 -0300

    desafio database queries finalizado

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -15,16 +15,23 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = await this.repository.findOneOrFail(user_id, { relations: ["games"] });
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM users ORDER BY first_name`); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      `SELECT * 
+      FROM users 
+      WHERE first_name = INITCAP('${first_name}') AND 
+      last_name = INITCAP('${last_name}')`  
+    ); // Complete usando raw query
   }
 }
